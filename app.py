0ef2c076495656d364bf5b0e75257c91c1fb16db
Olubukola <olubukade@yahoo.com>
2026-06-09 21:52:56 +0000

    Add health endpoint tests coverage and sonar config

--- app.py
+++ app.py
@@ -2,9 +2,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
     return "Homelab CI/CD Pipeline is Working!"
+
+@app.route("/health")
+def health():
+    return "healthy", 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
